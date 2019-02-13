d40f662730f0c08bffc44135fe4ac7b72226f4e3
Bernardo Gómez <a01209704@itesm.mx>
2019-02-13 16:31:05 +0000

    wip logistic

--- programs/logistic_regression/logistic_reg.py
+++ programs/logistic_regression/logistic_reg.py
@@ -1,26 +1,5 @@
 import fileinput
 from random import random
-from csv import reader
-
-
-# Load a CSV file
-def load_csv(filename):
-	dataset = list()
-	with open(filename, 'r') as file:
-		csv_reader = reader(file)
-		for row in csv_reader:
-			if not row:
-				continue
-			dataset.append(row)
-	for i in range(len(dataset[0])):
-	    str_column_to_float(dataset, i)
-	return dataset
-
-
-# Convert string column to float
-def str_column_to_float(dataset, column):
-	for row in dataset:
-		row[column] = float(row[column].strip())
 
 
 def initialize_params(num_params):
@@ -30,7 +9,7 @@ def initialize_params(num_params):
     return params
 
 
-def get_mean_error(params, x_set, y_set):
+def show_mean_error(params, x_set, y_set):
 	global __errors__
 	error_acum =0
 	for i in range(len(x_set)):
@@ -39,6 +18,7 @@ def get_mean_error(params, x_set, y_set):
 		error = y_hat - y_set[i]
 		error_acum += error**2 # this error is the original cost function, (the one used to make updates in GD is the derivated verssion of this formula)
 	mean_error_param = error_acum/len(x_set)
+	print("mean_error_param  %f " % mean_error_param)
 	return mean_error_param
 	
 
@@ -49,26 +29,27 @@ def eval_h(params, sample):
 	return acum;
 
 
-def train_model(l_rate, x_train_set, y_train_set, num_epochs):
-    params = initialize_params(len(x_train_set[0]))
+def train_model(num_x_cols, l_rate, x_train_set, y_train_set, num_epochs):
+    params = initialize_params(num_x_cols + 1)
     epoch = 0
     while epoch < num_epochs:
         epoch += 1
         old_params = list(params)
         for j in range(len(params)):
-            acum = 0
+            acum =0
             for i in range(len(x_train_set)):
                 acum += (eval_h(params,x_train_set[i]) - y_train_set[i]) * x_train_set[i][j] #Sumatory part of the Gradient Descent formula for linear Regression.
             params[j] = params[j] - l_rate*(1/len(x_train_set)) * acum  #Subtraction of original parameter value with learning rate included.
-        mean_error = get_mean_error(params, x_train_set, y_train_set)
-        print("epoch: %f, mean_error:  %f " % (epoch, mean_error))
+        mean_error_param = show_mean_error(params, x_train_set, y_train_set)
         if (old_params == params):
-            print("\nNo change in params, training finished")
+            print("No change in params, training finished")
             break
-        if (mean_error < 0.00001):
-            print("\nMin error reached")
+        if (mean_error_param < 0.00001):
+            print("Error is 0")
             break
-    print("epochs:  %f \nEnd of Training." % epoch)
+    print("epochs:  %f " % epoch)
+    print("final params: ")
+    print(params)
     return params
 
  
@@ -81,33 +62,25 @@ def test_model(x_test_set, params):
 
 
 def main():
+    file_input = fileinput.input()
     x_set = []
     y_set = []
     l_rate = 0.01
     num_epochs = 5000
-    
-    # load and prepare data
-    filename = 'data.csv'
-    dataset = load_csv(filename)
-    
-    # separate y and x set. Add bias column
-    for row in dataset:
-        y_set.append(row.pop())
-        row.append(1.0)
-        x_set.append(row)
-    
-    # train model
-    trained_params = train_model(l_rate, x_set, y_set, num_epochs)
-    print("\n-> Final params: ")
-    print(trained_params)
-    
-    # test model
-    result = test_model(x_set, trained_params)
-    print("-> Predictions: ")
-    print(result)
-    print("-> Real Values: ")
-    print(y_set)
 
+    num_x_cols = int(file_input[0])
+    data_size = int(file_input[1])
+    for i in range(data_size):
+        row_str = file_input[i + 2].replace(" ", "").replace("\t", "").replace("\n", "").split(',')
+        row_float = [float(x) for x in row_str]
+        y_set.append(row_float.pop())
+        row_float.append(1.0)
+        x_set.append(row_float)
+
+    trained_params = train_model(num_x_cols, l_rate, x_set, y_set, num_epochs)
+    result = test_model(x_set, trained_params)
+    print("Predictions: ")
+    print(result)
 
 
 if __name__ == "__main__":
